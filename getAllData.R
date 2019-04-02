fef10930618c2d1576d2ced77724a2cd6586acff
Sonia Garcia <43370296+SoniaRuiz@users.noreply.github.com>
2019-04-02 13:20:03 +0100

    last version

--- getAllData.R
+++ getAllData.R
@@ -1,83 +1,311 @@
 library("stringr")
 library(data.table)
 library("RSQLite")
-con = dbConnect(SQLite(), dbname="/data/splicing_tolerance/splicing_tolerance.sqlite")
 
-
-#######################################
-######## GET ALL TABLES ###############
-#######################################
-query <- dbSendQuery(con, "select * from sqlite_master;")
-all_tables <- dbFetch(query)
-dbClearResult(query)
-table_names <- all_tables$name
-gtex_table <- table_names[56]
-table_names <- table_names[-1] #Delete "Junc_coordinates" name-table
-table_names <- table_names[-55] #Delete "GTEX" name-table
-########################################
-########## GET GTEX DATA ###############
-########################################
-# Get all 'USE ME' people from GTEX project 
-query <- dbSendQuery(con, paste0("SELECT DISTINCT subj_id FROM '",gtex_table,"' WHERE smafrze = 'USE ME';"))
-people <- dbFetch(query)
-dbClearResult(query)
-# Get all samplesID from 'USE ME' people from GTEX project
-samplesID <- list()
-for(person in people$subj_id){
-  query<-dbSendQuery(con, paste0("SELECT sample_recount_id FROM '",gtex_table,"' WHERE subj_id = '",person,"';"))
-  samplesID[[person]] <- dbFetch(query)$sample_recount_id;
+getAllJunctionsAndCounts <- function(numberOfCounts=1){
+  con = dbConnect(SQLite(), dbname="/data/splicing_tolerance/splicing_tolerance.sqlite")
+  #######################################
+  ######## GET ALL TABLES ###############
+  #######################################
+  query <- dbSendQuery(con, "select * from sqlite_master;")
+  all_tables <- dbFetch(query)
   dbClearResult(query)
-}
-###############################################
-######## GET COLNAMES FROM EVERY TISSUE #######
-###############################################
-col_names <- list()
-for(table_name in table_names){
-  #Only select non-sex-specific tissues
-  if(!str_detect(tolower(table_name), "cells|testis|vagina|ovary|uterus|prostate|cervix|bladder|fallopian|breast")){
-    query<-dbSendQuery(con, paste0("PRAGMA table_info('",table_name,"');"))
-    columns <- dbFetch(query)$name
+  table_names <- all_tables$name
+  gtex_table <- table_names[56]
+  table_names <- table_names[-1] #Delete "Junc_coordinates" name-table
+  table_names <- table_names[-55] #Delete "GTEX" name-table
+  ########################################
+  ########## GET GTEX DATA ###############
+  ########################################
+  # Get all 'USE ME' people from GTEX project 
+  query <- dbSendQuery(con, paste0("SELECT DISTINCT subj_id FROM '",gtex_table,"' WHERE smafrze = 'USE ME';"))
+  people <- dbFetch(query)
+  dbClearResult(query)
+  # Get all samplesID from 'USE ME' people from GTEX project
+  samplesID <- list()
+  for(person in people$subj_id){
+    query<-dbSendQuery(con, paste0("SELECT sample_recount_id FROM '",gtex_table,"' WHERE subj_id = '",person,"';"))
+    samplesID[[person]] <- dbFetch(query)$sample_recount_id;
     dbClearResult(query)
-    #Only select those tissues with more than 75 samples
-    if(length(columns) >= 75)
-      col_names[[table_name]] <- columns[-1] #Delete juncID column
   }
-}
-###################################################
-######## GET ALL JUNCID PER PERSON PER TISSUE #####
-###################################################
-juncIDs_person_tissue <- list()
-juncIDs_tissue <- list()
-
-for(person in people$subj_id){
-  print(paste0("###### ", person, " ######"))
-  
-  for(table_name in table_names){
-    i<-1
-    for(sample in samplesID[[person]]){
-      
-      if(length(which(col_names[[table_name]] == sample)) == 1){ #every sampleID is unique, so should be equal to 1
-        
-        print(paste0("                 ###### ", table_name, " ######"))
-        query<-dbSendQuery(con, paste0("SELECT juncID FROM '",table_name,"' WHERE `",sample,"` >= 1 ;"))
-        juncIDs <- dbFetch(query)[[1]];
-        dbClearResult(query)
-        if(table_name %in% names(juncIDs_tissue))
-        {
-          name <- paste0(table_name, "_",i)
-          juncIDs_tissue[[name]]<- juncIDs
-          
-        }
-        else
-          juncIDs_tissue[[table_name]]<- juncIDs
-        
+  ###############################################
+  ######## GET COLNAMES FROM EVERY TISSUE #######
+  ###############################################
+  col_names <- list()
+  tissue_names <- NULL
+  for(tissue in table_names){
+    #Only select non-sex-specific tissues
+    if(!str_detect(tolower(tissue), "cells|testis|vagina|ovary|uterus|prostate|cervix|bladder|fallopian|breast")){
+      query<-dbSendQuery(con, paste0("PRAGMA table_info('",tissue,"');"))
+      columns <- dbFetch(query)$name
+      dbClearResult(query)
+      #Only select those tissues with more than 70 samples (71 because later I remove the juncID column)
+      if(length(columns) >= 71){
+        col_names[[tissue]] <- columns[-1] #Delete juncID column
+        tissue_names <- c(tissue_names,tissue)
       }
     }
   }
-  juncIDs_person_tissue[[person]] <- juncIDs_tissue
-  juncIDs_tissue <- NULL
+  ###################################################
+  ######## GET ALL JUNCID PER PERSON PER TISSUE #####
+  ###################################################
+  juncIDs_person_tissue <- list()
+  juncIDs_tissue <- list()
+  
+  for(person in people$subj_id){
+    print(paste0("###### ", person, " ######"))
+    
+    for(tissue in tissue_names){
+      i<-1
+      for(sample in samplesID[[person]]){
+        
+        if(length(which(col_names[[tissue]] == sample)) == 1 ){ #every sampleID is unique, so should be equal to 1
+          
+          print(paste0("                 ###### ", tissue, " ######"))
+          
+          query<-dbSendQuery(con, paste0("SELECT juncID,`",sample,"` FROM '",tissue,"' WHERE `",sample,"` >= ",numberOfCounts," ;"))
+          juncIDs <- dbFetch(query);
+          dbClearResult(query)
+          if(tissue %in% names(juncIDs_tissue))
+          {
+            name <- paste0(tissue, "_",i)
+            juncIDs_tissue[[name]]<- list(juncID = juncIDs[[1]], number = juncIDs[[2]])
+          }
+          else{
+            juncIDs_tissue[[tissue]]<- list(juncID = juncIDs[[1]], number = juncIDs[[2]])
+          }
+        }
+      }
+    }
+    juncIDs_person_tissue[[person]] <- juncIDs_tissue
+    juncIDs_tissue <- NULL
+  }
+  fileName <- paste0("~/R/splicing_project/SplicingProject/Results/allJunctions_NonSexSpecific_Counts",numberOfCounts,".rda")
+  saveRDS(list(juncIDs=juncIDs_person_tissue,tissues=tissue_names), fileName)
+  print("File saved!")
 }
-all_data <- list(samplesID_person=samplesID,col_names=col_names,juncIDs_person_tissue=juncIDs_person_tissue)
-saveRDS(all_data, "~/R/splicing_project/SplicingProject/Results/nonSexSpecificJunctions.rda")
-print("File saved!")
 
+getAllJunctionsAndCountsAdjusted <- function(sourceData="~/R/splicing_project/SplicingProject/Results/allJunctions_NonSexSpecific_Counts1.rda",
+                                             saveResults = T,
+                                             isMinimum = F,
+                                             fileName = "allJunctions_NonSexSpecific_Counts1_AdjustedMaximum.rda") {
+  
+  ## READ JUNCTIONS FILE 
+  all.JunctionsCount1 <-readRDS(file = sourceData)
+  
+  ## declare variables
+  allJunctionsCount1 <- all.JunctionsCount1$juncIDs
+  i<-1
+  threshold <- 0
+  if(isMinimum){
+    minimumCount<-NULL
+  }else
+    maximumCount<-0
+
+  
+  for(person in names(allJunctionsCount1)){
+    #print(paste0("###### ", person, " ######"))
+    
+    for(tissue in names(allJunctionsCount1[[person]])){
+      
+      #divide each cell by its column total
+      totalCounts <- sum(allJunctionsCount1[[person]][[tissue]][[2]])
+      allJunctionsCount1[[person]][[tissue]][[2]] <- allJunctionsCount1[[person]][[tissue]][[2]]/totalCounts
+      
+      ## GET THE MINIMUM NUMBER OF TOTAL COUNTS 
+      if(isMinimum){
+        if(i == 1 || totalCounts < minimumCount){
+          minimumCount <- totalCounts
+          threshold <- 3/minimumCount
+          print(paste0("New minimum: ",minimumCount,". Threshold: ", threshold))
+        }
+      }
+      else{
+        if(i == 1 || maximumCount < totalCounts){
+          maximumCount <- totalCounts
+          threshold <- 3/maximumCount
+          print(paste0("New maximum: ",maximumCount,". Threshold: ", threshold))
+        }
+      }
+      i<-i+1
+    }
+  }
+  if(saveResults){
+      saveRDS(object = list(juncID=allJunctionsCount1,threshold=threshold),
+              file = paste0("~/R/splicing_project/SplicingProject/Results/",fileName))
+      print("File saved!")
+  }
+}
+
+getJunctionsAndCountsAdjustedByThreshold <- function(sourceData="~/R/splicing_project/SplicingProject/Results/allJunctions_NonSexSpecific_Counts1_AdjustedMaximum.rda",
+                                                     saveResults = T,
+                                                     fileName = "allJunctions_NonSexSpecific_Counts3_AdjustedMaximum.rda") {
+  
+  
+  ###########################################################
+  ######## FILTER ALL JUNCTIONS BY USING A THRESHOLD ########
+  ###########################################################
+  
+  ## READ JUNCTIONS FILE 
+  all.junctions_adjusted <- readRDS(file = sourceData)
+  
+  threshold <- all.junctions_adjusted$threshold
+  all_junctions_adjusted <- all.junctions_adjusted$juncID
+ 
+  
+  ## START LOOPING
+  adjustedJuctionsToSave <- list()
+  tissuesToSave <- list()
+  
+  for(person in names(all_junctions_adjusted)){
+    
+    for(tissue in names(all_junctions_adjusted[[person]])){
+      
+      #only first tissue samples
+      if(str_sub(tissue,-2,-2) != "_"){
+        
+        #print(paste0("#################### ", person, " ########### ", tissue, " ####################"))
+        
+        #get all junctions which have more than three adjusted counts
+        indexAdjustedJunctionsCounts_GreaterThan3 <- which(all_junctions_adjusted[[person]][[tissue]]$number >= threshold)
+        print(length(indexAdjustedJunctionsCounts_GreaterThan3))
+        if(length(indexAdjustedJunctionsCounts_GreaterThan3) > 0){
+          #save results
+          tissuesToSave[[tissue]] <- list(juncID=all_junctions_adjusted[[person]][[tissue]]$juncID[indexAdjustedJunctionsCounts_GreaterThan3],
+                                          counts=all_junctions_adjusted[[person]][[tissue]]$number[indexAdjustedJunctionsCounts_GreaterThan3])
+        }
+      }
+    }
+    if(!is.null(tissuesToSave)){
+      adjustedJuctionsToSave[[person]] <- tissuesToSave
+      tissuesToSave <- list()
+    }
+  }
+  
+  if(saveResults){
+    saveRDS(adjustedJuctionsToSave, paste0("~/R/splicing_project/SplicingProject/Results/", fileName))
+    print("file saved!")
+  }
+}
+
+getUniqueAdjustedJunctions <- function(sourceData="allJunctions_NonSexSpecific_Counts3_AdjustedMinimum.rda",
+                                       fileName="uniqueJunctions_NonSexSpecific_Counts3_AdjustedMinimum.rda",
+                                       saveResults = T){
+  
+  ## READ JUNCTIONS FILE 
+  all_junctions_adjusted_GT3 <-readRDS(file = paste0("~/R/splicing_project/SplicingProject/Results/",sourceData))
+  
+  
+  adjustedUniqueJunctions <- list()
+  
+  for(person in names(all_junctions_adjusted_GT3)){
+    
+    adjustedUniqueJunctions[[person]] <- list()
+    
+    
+    for(tissue in names(all_junctions_adjusted_GT3[[person]])){
+      
+      if(str_sub(tissue,-2,-2) != "_"){
+        
+        print(paste0("#################### ", person, " ########### ", tissue, " ####################"))
+        
+        #set the initial data
+        uniqueJuncIDs <- all_junctions_adjusted_GT3[[person]][[tissue]]$juncID
+        
+        adjustedUniqueJunctions[[person]][[tissue]] <- list()
+        adjustedUniqueJunctions[[person]][[tissue]][[person]] <- list(juncID = uniqueJuncIDs, percentage = 100)
+        
+        ###########################################################
+        ######## CROSS PERSON1 DATA WITH THE OTHER PEOPLE
+        ######## COMPARE RESULTS --> INTERSECTION BETWEEN BOTH
+        ###########################################################
+        
+        for(personN in names(all_junctions_adjusted_GT3)){
+          
+          if(person != personN && length(all_junctions_adjusted_GT3[[personN]][[tissue]]) > 0){
+            
+            outerLeft <- setdiff(uniqueJuncIDs,all_junctions_adjusted_GT3[[personN]][[tissue]]$juncID)
+            
+            if(length(outerLeft) > 0){
+              uniqueJuncIDs <- outerLeft
+              
+              percentage <- length(uniqueJuncIDs)*100/length(all_junctions_adjusted_GT3[[person]][[tissue]]$juncID)
+              adjustedUniqueJunctions[[person]][[tissue]][[personN]] <- list(juncID = uniqueJuncIDs, percentage = percentage)
+              
+            }
+          }
+        }
+      }
+    }
+  }
+  if(saveResults){
+    saveRDS(adjustedUniqueJunctions, paste0("~/R/splicing_project/SplicingProject/Results/", fileName))
+    print("file saved!")
+  }
+}
+
+
+getRandomizedUniqueJunctionAdjusted <- function(sourceData="allJunctions_NonSexSpecific_Counts3_AdjustedMaximum.rda",
+                                                fileName="randomized_UniqueJunctions_NonSexSpecific_Counts3_AdjustedMaximum.rda",
+                                                saveResults = T){
+  ## READ JUNCTIONS FILE 
+  all_junctions_adjusted_GT3 <-readRDS(file = paste0("~/R/splicing_project/SplicingProject/Results/",sourceData))
+   
+
+  dataRandomizedPeople <- list()
+  for(item in 1:30){
+     
+    #Randomize people
+    people <- sample(names(all_junctions_adjusted_GT3),replace = F)
+    adjustedUniqueJunctions <- list()
+      
+    for(person in people){
+        
+        adjustedUniqueJunctions[[person]] <- list()
+        
+        
+        for(tissue in names(all_junctions_adjusted_GT3[[person]])){
+          
+          if(str_sub(tissue,-2,-2) != "_"){
+            
+            print(paste0("#################### ", person, " ########### ", tissue, " ####################"))
+            
+            #set the initial data
+            uniqueJuncIDs <- all_junctions_adjusted_GT3[[person]][[tissue]]$juncID
+            
+            adjustedUniqueJunctions[[person]][[tissue]] <- list()
+            adjustedUniqueJunctions[[person]][[tissue]][[person]] <- list(juncID = uniqueJuncIDs, percentage = 100)
+            
+            ###########################################################
+            ######## CROSS PERSON1 DATA WITH THE OTHER PEOPLE
+            ######## COMPARE RESULTS --> INTERSECTION BETWEEN BOTH
+            ###########################################################
+            
+            for(personN in people){
+              
+              if(person != personN && length(all_junctions_adjusted_GT3[[personN]][[tissue]]) > 0){
+                
+                outerLeft <- setdiff(uniqueJuncIDs,all_junctions_adjusted_GT3[[personN]][[tissue]]$juncID)
+                
+                if(length(outerLeft) > 0){
+                  uniqueJuncIDs <- outerLeft
+                  
+                  percentage <- length(uniqueJuncIDs)*100/length(all_junctions_adjusted_GT3[[person]][[tissue]]$juncID)
+                  adjustedUniqueJunctions[[person]][[tissue]][[personN]] <- list(juncID = uniqueJuncIDs, percentage = percentage)
+                  
+                }
+              }
+            }
+          }
+        }
+      }
+     dataRandomizedPeople[[item]] <- adjustedUniqueJunctions
+   }
+   if(saveResults){
+     saveRDS(dataRandomizedPeople, file = paste0("~/R/splicing_project/SplicingProject/Results/",fileName))
+     print("File saved!")
+   }
+ }
+
+
+getRandomizedUniqueJunctionAdjusted()
